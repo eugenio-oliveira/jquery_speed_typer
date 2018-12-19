a1586ffb1ab6e2e18314cd6651fcee086749ea8d
Eugênio Oliveira <eugenio@infoway-pi.com.br>
2018-12-19 18:00:08 -0300

    add counters

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 var phrase = $('.phrase').text();
-var wordSize = phrase.split(" ").length;
-var phraseSize =  $("#phraseSize");
+var wordSize = phrase.split(' ').length;
+var phraseSize =  $('#phraseSize');
 phraseSize.text(wordSize);
+
+var field = $('.type-field');
+field.on('input', function(){
+  var content = field.val();
+  var wordQty = content.split(/\S+/).length - 1;
+  $('#word-counter').text(wordQty);
+  var characterQty = content.length;
+  $('#character-counter').text(characterQty);
+});
