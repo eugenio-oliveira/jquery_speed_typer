6c57d0877ae7980c62d717550962a3b3b3b3f204
Eugênio Oliveira <eugenio@infoway-pi.com.br>
2018-12-26 16:04:45 -0300

    Game Over Com Eventos

--- js/main.js
+++ js/main.js
@@ -4,10 +4,24 @@ var phraseSize =  $('#phraseSize');
 phraseSize.text(wordSize);
 
 var field = $('.type-field');
+
 field.on('input', function(){
   var content = field.val();
   var wordQty = content.split(/\S+/).length - 1;
   $('#word-counter').text(wordQty);
   var characterQty = content.length;
   $('#character-counter').text(characterQty);
+});
+
+var remainingTime = $("#type-timer").text();
+
+field.one("focus", function(){
+ var chronometerID = setInterval(function(){
+    remainingTime--;
+    $("#type-timer").text(remainingTime);
+    if(remainingTime < 1){
+      field.attr("disabled", true);
+      clearInterval(chronometerID);
+    }
+  }, 1000);
 });
