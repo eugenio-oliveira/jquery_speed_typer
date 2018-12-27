7d4a9a7eb1be12ff49f930afdd530f3d049ea192
Eugênio Oliveira <eugenio@infoway-pi.com.br>
2018-12-27 11:56:38 -0300

    add restart function

--- js/main.js
+++ js/main.js
@@ -1,27 +1,57 @@
-var phrase = $('.phrase').text();
-var wordSize = phrase.split(' ').length;
-var phraseSize =  $('#phraseSize');
-phraseSize.text(wordSize);
+var intialTime = $("#type-timer").text();
+
+$(document).ready(function(){
+  updatePhraseSize();
+  startCounters();
+  startTimer();
+});
+
+function updatePhraseSize(){
+  var phrase = $('.phrase').text();
+  var wordSize = phrase.split(' ').length;
+  var phraseSize =  $('#phraseSize');
+  phraseSize.text(wordSize);
+}
 
 var field = $('.type-field');
 
-field.on('input', function(){
-  var content = field.val();
-  var wordQty = content.split(/\S+/).length - 1;
-  $('#word-counter').text(wordQty);
-  var characterQty = content.length;
-  $('#character-counter').text(characterQty);
-});
+function startCounters(){
+    field.on('input', function(){
+    var content = field.val();
+    var wordQty = content.split(/\S+/).length - 1;
+    $('#word-counter').text(wordQty);
+    var characterQty = content.length;
+    $('#character-counter').text(characterQty);
+  });
 
-var remainingTime = $("#type-timer").text();
+}
 
-field.one("focus", function(){
- var chronometerID = setInterval(function(){
-    remainingTime--;
-    $("#type-timer").text(remainingTime);
-    if(remainingTime < 1){
-      field.attr("disabled", true);
-      clearInterval(chronometerID);
-    }
-  }, 1000);
+
+
+function startTimer(){
+    var remainingTime = $("#type-timer").text();
+    field.one("focus", function(){
+   var chronometerID = setInterval(function(){
+      remainingTime--;
+      $("#type-timer").text(remainingTime);
+      if(remainingTime < 1){
+        field.attr("disabled", true);
+        clearInterval(chronometerID);
+      }
+    }, 1000);
+  });
+}
+
+function restartGame(){
+  field.attr("disabled", false);
+  field.val("");
+  $('#word-counter').text("0");
+  $('#character-counter').text("0");
+  $("#type-timer").text(intialTime);
+  startTimer();
+}
+
+
+$("#button-reset").click(function(){
+  restartGame();
 });
