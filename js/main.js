59ff8fe1d7cca6e89844f1e439ec75c3989699f3
Eugênio Oliveira <eugenio@infoway-pi.com.br>
2018-12-28 10:00:39 -0300

    add line comments

--- js/main.js
+++ js/main.js
@@ -1,35 +1,37 @@
 var intialTime = $("#type-timer").text();
 
+//Ao carregar a página, carrega-se todas as funções necessárias para o aplicativo
 $(document).ready(function(){
   updatePhraseSize();
   startCounters();
   startTimer();
 });
 
+//Função para atualizar o tamanho da frase
 function updatePhraseSize(){
-  var phrase = $('.phrase').text();
-  var wordSize = phrase.split(' ').length;
-  var phraseSize =  $('#phraseSize');
-  phraseSize.text(wordSize);
+  var phrase = $('.phrase').text(); //Pega o texto que deve ser digitado pelo usuário
+  var wordSize = phrase.split(' ').length; //Separa o texto por palavras através dos espaços
+  var phraseSize =  $('#phraseSize'); //Guarda o tamanho da frase
+  phraseSize.text(wordSize); //Exibe o tamanho da frase
 }
 
 var field = $('.type-field');
 
+//Função para iniciar os contadores
 function startCounters(){
     field.on('input', function(){
-    var content = field.val();
-    var wordQty = content.split(/\S+/).length - 1;
-    $('#word-counter').text(wordQty);
-    var characterQty = content.length;
-    $('#character-counter').text(characterQty);
+    var content = field.val(); //Pega o conteúdo escrito no campo
+    var wordQty = content.split(/\S+/).length - 1; //Verifica se as palavras estão separadas por espaço
+    $('#word-counter').text(wordQty); //Insere a quantidade de palavras escritas
+    var characterQty = content.length; //Pega o tamanho de letras escrito pelo usuário
+    $('#character-counter').text(characterQty); //Exibe a quantidade de letras escritas pelo usuário
   });
 
 }
 
-
-
+//Função para inciar o timer
 function startTimer(){
-    var remainingTime = $("#type-timer").text();
+    var remainingTime = $("#type-timer").text(); //Pega o tempo descrito no span
     field.one("focus", function(){
    var chronometerID = setInterval(function(){
       remainingTime--;
@@ -42,16 +44,17 @@ function startTimer(){
   });
 }
 
+//Função para reiniciar o jogo
 function restartGame(){
-  field.attr("disabled", false);
-  field.val("");
-  $('#word-counter').text("0");
-  $('#character-counter').text("0");
-  $("#type-timer").text(intialTime);
-  startTimer();
+  field.attr("disabled", false); //seta  atributo "disabled" para false
+  field.val(""); //zera o conteúdo do campo de texto
+  $('#word-counter').text("0"); //Reinicia o contador de palavras
+  $('#character-counter').text("0"); // Reinicia o contador de letras
+  $("#type-timer").text(intialTime); //Zera o tempo do contador
+  startTimer(); //(Re)Inicia o Contador
 }
 
-
+//Ao clicar no botão, o jogo é reiniciado
 $("#button-reset").click(function(){
   restartGame();
 });
